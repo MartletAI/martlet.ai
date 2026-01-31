991a75c027dc66c4767b4a868143e94b0db350c0
RiyasaatAhmed <riyasaatahmed24@gmail.com>
2026-02-01 00:04:36 +0600

    feat: integrate vercel speed insights for performance monitoring.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar, Footer } from "@/components";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );

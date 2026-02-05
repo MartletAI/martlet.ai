c974a32cb3f120f988391c0823cdeec11cd9c0c1
RiyasaatAhmed <riyasaatahmed24@gmail.com>
2026-02-06 04:31:29 +0600

    feat: enhance SEO metadata by adding keywords and updating titles/descriptions across several pages.

--- src/app/solutions/radv/page.tsx
+++ src/app/solutions/radv/page.tsx
@@ -9,6 +9,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "RADV Audits Solution for Compliance | Martlet AI",
   description: "Ensure precise risk score validation and compliance with Martlet AI's RADV audits solutions, enhancing audit accuracy and efficiency.",
+  keywords: ["radv", "radv audits", "radv audit", "radv audit in risk adjustment", "risk adjustment data validation", "radv cms", "radv coding", "radv risk adjustment", "radv regulation services", "radv audit services"],
 };
 
 export default function Page() {
--- src/app/solutions/retrospective-risk-adjustment/page.tsx
+++ src/app/solutions/retrospective-risk-adjustment/page.tsx
@@ -9,6 +9,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Retrospective Risk Adjustment Solution | Martlet AI",
   description: "Boost payer performance with Martlet AI’s retrospective risk adjustment solutions for precise coding, audits, and optimized healthcare risk scores.",
+  keywords: ["retrospective risk adjustment", "retrospective coding", "retrospective risk adjustment services"],
 };
 
 export default function Page() {

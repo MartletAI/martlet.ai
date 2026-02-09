4cdb217f82d2f345fdc563c08539e3d398190fc1
RiyasaatAhmed <riyasaatahmed24@gmail.com>
2026-02-10 04:02:58 +0600

    feat: update WVU Medicine video embed to use `youtube-nocookie.com` domain.

--- src/app/(landing-page)/components/industry-partners/wvu-medicine.tsx
+++ src/app/(landing-page)/components/industry-partners/wvu-medicine.tsx
@@ -34,7 +34,7 @@ export function WVUMedicine() {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/6jTyxiREvVE"
+            src="https://www.youtube-nocookie.com/embed/6jTyxiREvVE"
             title="WVU Medicine video"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
